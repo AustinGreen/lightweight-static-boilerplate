52dab636dc71daa341438e58162eb33ea6b88fc3
Austin Green <austinzgreen@gmail.com>
2016-08-13 14:01:33 -0400

    added header element and appear effect for nav

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ gem "middleman", "~>3.3.12"
 gem "middleman-livereload", "~> 3.1.0"
 gem "jquery-middleman"
 gem "middleman-minify-html"
-gem 'font-awesome-sass'
 gem 'image_optim'
 gem 'middleman-imageoptim'
 gem "middleman-sprockets"
--- source/js/all.js
+++ source/js/all.js
@@ -2,17 +2,34 @@
 //= require_tree .
 
 // Smooth Scroll
-$(function() {
-  $('a[href*=#]:not([href=#music-gallery])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+// $(function() {
+//   $('a[href*=#]').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html,body').animate({
+//           scrollTop: target.offset().top
+//         }, 1000);
+//         return false;
+//       }
+//     }
+//   });
+// });
+
+// Hide and show nav
+$(document).ready(function(){
+  // hide .navbar first
+  $(".navbar-fixed").hide();
+  
+  // fade in .navbar
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > $(window).height()) {
+        $('.navbar-fixed').fadeIn();
+      } else {
+        $('.navbar-fixed').fadeOut();
       }
-    }
+    });
   });
 });
